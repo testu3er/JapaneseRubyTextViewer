0d2cfdebadf4b35101c023790995335910fab6fe
testu3er <testu3er@github.com>
2020-01-13 16:38:27 +0900

    first commit

---
+++ JapaneseRubyTextViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JapaneseRubyTextViewer
+//
+//  Created by mini2019 on 2020/01/13.
+//  Copyright © 2020 mini2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
